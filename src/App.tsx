a2acd96a2f992f7c865ecbfc925c53da906922e7
saritakarwaa <karwasarita672@gmail.com>
2025-08-04 00:09:21 +0530

    some tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Education from "./components/Education";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Education />
     </>
   );
 }
