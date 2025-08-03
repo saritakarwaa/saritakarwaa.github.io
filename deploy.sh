8cc7c8eac2b5979e33868bf53ffe016b97c78ed2
saritakarwaa <karwasarita672@gmail.com>
2025-08-04 00:22:38 +0530

    some more tweaks

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,10 @@ echo "🧹 Cleaning old static files from root..."
 rm -rf assets index.html *.js *.css *.map
 
 echo "📦 Copying new build files to root..."
-cp -r dist/* ./
+cp -r dist/* ./ 
 
 echo "🗑️ Removing dist folder..."
 rm -rf dist
 
 echo "✅ Deployment files are ready in root!"
+
